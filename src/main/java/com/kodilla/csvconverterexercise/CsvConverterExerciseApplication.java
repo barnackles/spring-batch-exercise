2c6be3f75aaa10bf76d10d3509ff0ca5f44ccdd1
barnackles <p.martewicz@yahoo.com>
2023-05-28 12:53:07 +0200

    Project and source file setup.

---
+++ src/main/java/com/kodilla/csvconverterexercise/CsvConverterExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.csvconverterexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvConverterExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvConverterExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodilla/csvconverterexercise/CsvConverterExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.csvconverterexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvConverterExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
